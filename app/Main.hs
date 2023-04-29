a1a14ae5da31a266589831637aeb38c0e7bc6f0c
AugmenTab <tbaum92@live.com>
2023-04-29 15:51:40 -0500

    Launches window with label
    The application now runs, launching a blank window with the provided
    label and basic control buttons.

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,21 @@ module Main
 
 import           Flipstone.Prelude
 
-import qualified Data.Text.IO as IO
+import qualified GI.Gtk as Gtk
 
 main :: IO ()
 main = do
-  IO.putStrLn "Hello, world!"
+  _ <- Gtk.init Nothing
+
+  window <- Gtk.windowNew Gtk.WindowTypeToplevel
+  Gtk.setContainerBorderWidth window 10
+  Gtk.setWindowTitle window "High Command - Generate Missions for Halo: Mythic"
+  Gtk.setWindowResizable window False
+  Gtk.setWindowDefaultWidth window 700
+  Gtk.setWindowDefaultHeight window 700
+  Gtk.setWindowWindowPosition window Gtk.WindowPositionCenter
+
+  _ <- Gtk.onWidgetDestroy window Gtk.mainQuit
+  Gtk.widgetShowAll window
+
+  Gtk.main
